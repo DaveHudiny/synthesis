319f7f1ffe218101072d4225b1b2bcd8426ed27f
DaveHudiny <skolahudak@gmail.com>
2023-12-20 16:41:52 +0100

    Added tests

--- rl_approach/runner.sh
+++ rl_approach/runner.sh
@@ -1,3 +1,14 @@
+python shield_v2.py --grid-model "obstacle" --learning_method "SAC" --eval-episodes 10 --constants "N=6" 
+python shield_v2.py --grid-model "intercept" --learning_method "SAC" --eval-episodes 10 --constants "N=6,RADIUS=2"
+python shield_v2.py --grid-model "rocks" --learning_method "SAC" --eval-episodes 10 --constants "N=6"
+python shield_v2.py --grid-model "refuel" --learning_method "SAC" --eval-episodes 10 --constants "N=6,ENERGY=10"
+python shield_v2.py --grid-model "evade" --learning_method "SAC" --eval-episodes 10 --constants "N=6,RADIUS=2"
+python shield_v2.py --grid-model "avoid" --learning_method "SAC" --eval-episodes 10 --constants "N=6"
+
+exit 0
+
+python shield_v2.py --grid-model "obstacle" --learning_method "SAC" --eval-episodes 10 --constants "N=6" > logs/obstacle_sac_shield.log
+python shield_v2.py --grid-model "obstacle" --learning_method "DDQN" --eval-episodes 10 --constants "N=6" > logs/obstacle_ddqn_shield.log
 python shield_v2.py --grid-model "intercept" --learning_method "SAC" --eval-episodes 10 --constants "N=6,RADIUS=2" > logs/intercept_sac_shield.log
 python shield_v2.py --grid-model "intercept" --learning_method "DDQN" --eval-episodes 10 --constants "N=6,RADIUS=2" > logs/intercept_ddqn_shield.log
 python shield_v2.py --grid-model "rocks" --learning_method "SAC" --eval-episodes 10 --constants "N=6" > logs/rocks_sac_shield.log
@@ -9,7 +20,3 @@ python shield_v2.py --grid-model "evade" --learning_method "DDQN" --eval-episode
 python shield_v2.py --grid-model "avoid" --learning_method "SAC" --eval-episodes 10 --constants "N=6" > logs/avoid_sac_shield.log
 python shield_v2.py --grid-model "avoid" --learning_method "DDQN" --eval-episodes 10 --constants "N=6" > logs/avoid_ddqn_shield.log
 
-exit 0
-
-python shield_v2.py --grid-model "obstacle" --learning_method "SAC" --eval-episodes 10 --constants "N=6" > logs/obstacle_sac_shield.log
-python shield_v2.py --grid-model "obstacle" --learning_method "DDQN" --eval-episodes 10 --constants "N=6" > logs/obstacle_ddqn_shield.log
