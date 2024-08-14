Fixed bug with wrong dimensions

rl_src/agents/networks/value_networks.py
@@ -95,24 +95,12 @@ class FSC_Critic(network.Network):
     
     def unbatched_belief_computation(self, beliefs):
         qvalues_table = tf.expand_dims(self.qvalues_table, axis=0)
-        expanded_values = tf.tile(qvalues_table, [beliefs.shape[0], 1, 1])
-        expanded_beliefs = tf.expand_dims(beliefs, axis=2)
-        values = tf.multiply(expanded_values, expanded_beliefs)
+        beliefs = tf.expand_dims(beliefs, axis=0)
+        beliefs = tf.transpose(beliefs, perm=[1, 2, 0])
+        values = tf.multiply(qvalues_table, beliefs)
         values = tf.reduce_mean(values, axis=1)
         values = tf.reduce_max(values, axis=-1)
         values = tf.expand_dims(values, axis=0)
-        return values
-    
-    # def batched_belief_computation(self, beliefs):
-        qvalues_table = tf.expand_dims(self.qvalues_table, axis=0)
-        qvalues_table = tf.tile(qvalues_table, [beliefs.shape[1], 1, 1])
-        qvalues_table = tf.expand_dims(qvalues_table, axis=0)
-        expanded_values = tf.tile(qvalues_table, [beliefs.shape[0], 1, 1, 1])
-        expanded_beliefs = tf.expand_dims(beliefs, axis=2)
-        expanded_beliefs = tf.transpose(expanded_beliefs, perm=[0, 1, 3, 2])
-        values = tf.multiply(expanded_values, expanded_beliefs)
-        values = tf.reduce_mean(values, axis=-2)
-        values = tf.reduce_max(values, axis=-1)
         return values
     
     def batched_belief_computation(self, beliefs): 
@@ -149,9 +137,11 @@ class FSC_Critic(network.Network):
             beliefs = self.belief_updater.compute_beliefs_for_consequent_steps(belief, indices)
             if len(tf.squeeze(indices).shape) == 1:
                 values = self.unbatched_belief_computation(beliefs)
+                belief = beliefs[-1, :]
             else:
                 values = self.batched_belief_computation(beliefs)
-            belief = beliefs[:, -1, :]
+                belief = beliefs[:, -1, :]
+            
         return values, belief
 
     def call(self, observations, step_type, network_state, training=False):

rl_src/tools/belief_updater.py
@@ -16,6 +16,13 @@ class Belief_Updater:
         self.pomdp_get_choice_index = stormpy_model.get_choice_index
         self.pomdp_transition_matrix = stormpy_model.transition_matrix
         self.init_observation_state_mapping()
+        self.init_observation_action_lengths()
+        
+    def init_observation_action_lengths(self):
+        self.observation_action_lengths = np.zeros((self.nr_observations,), dtype=np.int32)
+        for obs in range(len(self.action_labels_at_observation)):
+            self.observation_action_lengths[obs] = len(self.action_labels_at_observation[obs])
+        self.observation_action_lengths = tf.convert_to_tensor(self.observation_action_lengths, dtype=tf.int32)
         
     def init_observation_state_mapping(self):
         observation_state_mapping = np.zeros((self.nr_observations, self.nr_states))
@@ -52,6 +59,12 @@ class Belief_Updater:
             indices = observations[0]
             belief = self.observation_state_mapping[indices]
         return belief    
+    
+    def update_belief(self, belief, next_obs): # Update single belief state
+        new_belief = belief
+        for i in range(self.observation_action_lengths[next_obs]):
+            pass
+        return new_belief 
         
     @tf.function
     def compute_beliefs_for_consequent_steps(self, belief, observations, actions = None):
@@ -64,15 +77,19 @@ class Belief_Updater:
             for i in range(len(observations)): # Iterate over batches
                 sub_beliefs = []
                 sub_belief = belief[i]
-                for j in range(len(observations[i])): # Iterate over time steps
-                    # belief[i] = self.next_belief(belief, actions[i][j], observations[i][j])
+                sub_beliefs.append(sub_belief)
+                for j in range(len(observations[i]) - 1): # Iterate over time steps
+                    sub_belief = self.update_belief(sub_belief, observations[i, j+1])
                     sub_beliefs.append(sub_belief)
                 # belief = self.next_belief(belief, actions[i], observations[i])
                 beliefs.append(sub_beliefs)
         else:
-            for i in range(len(observations)):
+            beliefs.append(belief)
+            for i in range(len(observations) - 1):
+                belief = self.update_belief(belief, observations[i+1])
                 beliefs.append(belief)
-        return tf.convert_to_tensor(beliefs)
+        belief_tensor = tf.convert_to_tensor(beliefs)
+        return belief_tensor
         
     
     # @tf.function